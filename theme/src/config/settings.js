3efb658de52a7a8ab824a718645c8cb587a0fcf2
Anirban Sikdar <anirban9293@gmail.com>
2022-11-27 13:15:57 +0530

    site changes

--- theme/src/config/settings.js
+++ theme/src/config/settings.js
@@ -1,7 +1,7 @@
 export default {
 	title: `Anirban.io | A Personal Website for All to know about Me.`,
 	description: `A simple, clean, and modern page for All known and unknown to share about me.`,
-	url: `https://anirban-1009.github.io`, // No trailing slash!
+	url: `https://anirban-1009.netlify.app/`, // No trailing slash!
 	name: `MySite`, // The short name of the business or brand name. Used for things like the copyright in the footer.
   enableThemeSwitcher: false,
   showLittleSticksPlug: false, // Disable this if you want to remove the Little Sticks plug from the footer. (╯°□°)╯︵ ┻━┻
