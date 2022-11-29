f681270769ed29db000a15afe5c39d5218fce3a9
Anirban Sikdar <anirban9293@gmail.com>
2022-11-29 14:49:15 +0530

    Domain change

--- theme/astro.config.mjs
+++ theme/astro.config.mjs
@@ -7,7 +7,7 @@ import lit from "@astrojs/lit";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://anirban-1009.github.io/',
+  site: 'https://anirban-1009.netlify.app/',
   // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
   sitemap: true,
   // Generate sitemap (set to "false" to disable)
