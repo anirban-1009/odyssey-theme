8a1aea00d170a71348cf3ac1ca6291e303ea02dd
anirban-1009 <anirban9293@gmail.com>
2023-05-17 17:48:00 +0530

    Adding eco-friendly

--- theme/src/config/settings.js
+++ theme/src/config/settings.js
@@ -2,7 +2,7 @@ export default {
 	title: `Anirban.codes | A Personal Website for All to know about Me.`,
 	description: `A simple, clean, and modern page for All known and unknown to share about me.`,
 	url: `https://anirban.codes/`, // No trailing slash!
-	name: `Anirban.codes`, // The short name of the business or brand name. Used for things like the copyright in the footer.
+	name: `anirban.codes`, // The short name of the business or brand name. Used for things like the copyright in the footer.
   enableThemeSwitcher: false,
   showLittleSticksPlug: false, // Disable this if you want to remove the Little Sticks plug from the footer. (╯°□°)╯︵ ┻━┻
 };
